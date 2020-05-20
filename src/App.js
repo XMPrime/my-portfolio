311cb56fe56c8f8f98cd5a0eb1a6cbf5c49c83d2
Jason Chen <jason.l.chen@gmail.com>
2020-05-20 13:52:15 -0700

    fixed 404 error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// React Required
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeParticles from "./home/HomeParticles.jsx";
+import Error404 from "./elements/error404";
+
+import "./index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename='/'>
+      {/* <HomeParticles /> */}
+      <Switch>
+        <Route exact path='/' component={HomeParticles} />
+        <Route component={Error404} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
 // React Required
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-// Create Import File
-import "./index.scss";
-
-// Common Layout
-// import Layout from "./component/common/App";
-
-import HomeParticles from "./home/HomeParticles.jsx";
-
-import Error404 from "./elements/error404";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
-class Root extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`}>
-            <HomeParticles />
-          </Route>
-          <Route path={`${process.env.PUBLIC_URL}/404`}>
-            <Error404 />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
-}
+import App from "./App";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
